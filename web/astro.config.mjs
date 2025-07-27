a50999f62e7b10e8cce2eab3a70e8cc677b97199
R. Ilham Sastronegoro <bohrium.bearded_64@icloud.com>
2025-07-27 10:37:00 +0000

    add image support

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from 'astro/config'
 import node from '@astrojs/node'
 
+const CONTENT_ENDPOINT = process.env.CONTENT_ENDPOINT
+
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
     mode: 'standalone'
   }),
+  image: {
+    domains: [new URL(CONTENT_ENDPOINT).hostname]
+  },
   experimental: {
     liveContentCollections: true,
     rawEnvValues: true
